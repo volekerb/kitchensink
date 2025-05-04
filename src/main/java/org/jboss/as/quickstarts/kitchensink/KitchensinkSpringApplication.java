3558e19b4f65f032483073c0ff3b124dba504d11
Vsevolod <vsevolod.brekelov@gmail.com>
2025-05-04 22:14:20 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/KitchensinkSpringApplication.java
@@ -0,0 +1,13 @@
+package org.jboss.as.quickstarts.kitchensink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensinkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchensinkSpringApplication.class, args);
+	}
+
+}
